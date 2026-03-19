7c6764c18d16c469c9f059cb88b3f48cca5261be
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-03-19 21:07:25 +0530

    Add files via upload

---
+++ _3large.java
@@ -0,0 +1,21 @@
+public class _3large {
+    public static void main(String[]args){
+        int a =67;
+        int b=67765;
+        int c=356;
+        if (a>b&&a>c) {
+            System.out.println(a + " is greatest ");
+        }
+        else if (b>a&&b>c) {
+            System.out.println(b + " is greatest ");
+        }
+        else {
+
+            System.out.println(c + " is greatest ");
+        }
+
+
+
+    }
+
+}
