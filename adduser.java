fc01330a09c721799bc86de7a2891ce399e6badc
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-03-19 21:07:49 +0530

    Add files via upload

---
+++ adduser.java
@@ -0,0 +1,15 @@
+import java .util.*;
+public class adduser {
+    public static  void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the first number:");
+
+        int a=sc.nextInt();
+        System.out.println("enter the second number:");
+        int b =sc.nextInt();
+        System.out.println("the sum of a and b is :"+(a+b));
+        sc.close();
+
+
+    }
+}
