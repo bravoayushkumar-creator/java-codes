513d573750ff90ac38d41568149804965d9f55e4
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-03-22 12:02:18 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,13 @@
+public class array2 {
+    public static void main (String[]args){
+        int [] marks= new int[3];
+        marks[0]=98;
+        marks[1]=99;
+        marks[2]=101;
+        for(int i=0;i< marks.length;i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+
+}
