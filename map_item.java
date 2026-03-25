21ec72b6fc7c35c247e47fa197325cdc10ee2c88
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-03-25 06:37:58 +0530

    Add files via upload

---
+++ map_item.java
@@ -0,0 +1,15 @@
+//import java.util.List;
+//import java.util.ArrayList;
+//import java.util.*;
+//
+//public class map_item {
+//    List<String> values= Arrays.asList("yt","yu","hg");
+//
+//        System.out.println(values);
+//    List<String> result = values.stream()
+//            .map(item->)
+//
+//    //List<Integer> result = numbers.stream().map(n->n+5).toList()
+//    //List<Integer> result=numbers.stream().map(null);
+//        System.out.println(result);
+//}
