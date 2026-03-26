36ea310c7118cb33050b1ec4f9b9e65332da9b22
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-03-26 22:36:13 +0530

    Add files via upload

---
+++ map2.java
@@ -0,0 +1,15 @@
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Scanner;
+//public class map2 {
+//    public static void main(String[] args){
+//        List<String> names=Arrays.asList("abh","bg","grhg");
+//            System.out.println(names);
+//        List<String> result=names.stream()
+//                .map(name->.toUpperCase())
+//                .toList();
+//        System.out.println(result);
+//
+//    }
+//
+//}
