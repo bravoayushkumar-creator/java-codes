a2ace2dd5910d19480672330ee880c2dc176ccf2
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-04-05 22:27:33 +0530

    Add files via upload

---
+++ reduce_3.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.*;
+
+public class reduce_3 {
+    public static void main(String[] args){
+        List<Integer> list= Arrays.asList(34,56,45,76,67,35,67);
+        double avg =list.stream().reduce(0,(a,b)->a+b)/list.size();
+        System.out.println("avg is "+avg);
+    }
+}
