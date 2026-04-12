b925bdd2f556819ec43d93fe21ad9fd0a8a76013
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-04-12 23:52:30 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class largest {
+    public static void main(String[]args){
+        int a =8098;
+        int b=7868;
+        if (a>b)
+            System.out.println(a+" is greatest  ");
+        else
+            System.out.println(b+"is greatest");
+    }
+}
