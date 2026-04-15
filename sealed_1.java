e84bb9381e6d1faa4a90f65557bc5bb9adadf7ce
bravoayushkumar-creator <bravoayushkumar@gmail.com>
2026-04-15 23:46:51 +0530

    Add files via upload

---
+++ sealed_1.java
@@ -0,0 +1,19 @@
+//import java.awt.*;
+
+sealed class shape permits Circle,Rectangle{
+
+
+
+}
+ final class Circle extends shape{
+
+}
+ non-sealed class Rectangle extends shape {
+
+}
+public class sealed_1 {
+    public static void main(String[] args){
+
+    }
+
+}
